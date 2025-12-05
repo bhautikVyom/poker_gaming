3a7dd17636b335037367aa2b4edbca40c698e76e
Denish <denish.personal@gmail.com>
2025-12-05 17:16:54 +0530

    Common Button Add

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Logo from "../../assets/images/logo.png";
+import SheetMenu from "./SheetMenu";
+import HeaderMenu from "./HeaderMenu";
+import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="bg-primary py-3 lg:py-5 sticky top-0 z-20">
+      <div className="container">
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-10">
+            <div className="lg:hidden">
+              <SheetMenu />
+            </div>
+            <div className="">
+              <Image
+                src={Logo}
+                alt="Logo"
+                className="h-9 lg:h-10 w-[93px] lg:w-[104px]"
+              />
+            </div>
+            <HeaderMenu />
+          </div>
+          <div className=""><Button></Button></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- components/layout/HeaderMenu.tsx
+++ components/layout/HeaderMenu.tsx
@@ -1,9 +1,24 @@
+"use client"
+
 import React from 'react'
+
+cosnt MenuData = [
+  { name: 'Home', link: '/' },
+  { name: 'STORE', link: '/store' },
+  { name: 'DAILY STREAK', link: '/daily-streak' },
+  { name: 'ABOUT', link: '/about-us' },
+  { name: 'GUIDES', link: '/guides' },
+  { name: 'HELP', link: 'https://zyngasupport.helpshift.com/hc/en/27-zynga-poker/' },
+];
 
 const HeaderMenu = () => {
   return (
-    <div>
-      
+    <div className=''>
+      <ul className='grid lg:flex items-center gap-5 lg:gap-10'>
+        {MenuData.map((item, index) => (
+        <li className='' key={index}><a href={item.link} className='text-xl font-semibold'>{item.name}</a></li>
+        ))}
+      </ul>
     </div>
   )
 }
