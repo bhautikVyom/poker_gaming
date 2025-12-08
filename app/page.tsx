02c984ab328b5a1340c48b6e772fdc96496cef93
bhautikVyom <bhautik.vyom@gmail.com>
2025-12-08 15:28:03 +0530

    pages changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import Image from "next/image";
-import LeandingPage from "./leanding/page";
-import Faqs from "./leanding/faqs";
+import Faqs from "../components/common/faqs";
+import LeandingPage from "@/pages/leanding";
 
 export default function Home() {
   return (
     <>
       <Header />
       <div className="min-h-[calc(100dvh-439px)]">
-         <LeandingPage />
-         <Faqs />
+        <LeandingPage />
+        <Faqs />
       </div>
       <Footer />
     </>
---
+++ pages/leanding.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
+import Link from "next/link";
+import ConnectButton from "@/components/common/ConnectButton";
+import PokerPlus from "@/components/common/PokerPlus";
+import Chips from "@/components/common/Chips";
+
+const LeandingPage = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div>
+            <ConnectButton setIsOpen={setIsOpen} isOpen={isOpen} />
+
+            <div className="flex items-center justify-center gap-4 bg-[#1d1d1d] p-2.5">
+                <Link
+                    href="#chips"
+                    className="uppercase bg-primary text-background font-bold px-5 py-1.5 rounded-4xl text-lg"
+                >
+                    Chips
+                </Link>
+                <Link
+                    href="#gold"
+                    className="uppercase bg-primary text-background font-bold px-5 py-1.5 rounded-4xl text-lg"
+                >
+                    gold
+                </Link>
+            </div>
+
+            <PokerPlus />
+
+            <div className="bg-primary py-10 lg:py-24">
+                <div className="container">
+                    <p className="text-background/80 italic lg:text-lg">
+                        ACTIVE ZYNGA POKER ACCOUNT REQUIRED. 20% more chips and gold
+                        compared with same price purchases in device app store versions of
+                        ZYNGA POKER. Chips and gold values shown after login are inclusive
+                        of any purchase multipliers that are applicable to you.
+                    </p>
+                </div>
+            </div>
+
+            <section id="chips">
+                <Chips />
+            </section>
+        </div>
+    );
+};
+
+export default LeandingPage;
