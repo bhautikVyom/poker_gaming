2a14a2c413a36cc3ab8587ef476c1ac39169b92e
Denish <denish.personal@gmail.com>
2025-12-08 15:06:29 +0530

    Change

--- app/leanding/page.tsx
+++ app/leanding/page.tsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button";
 import React, { useState } from "react";
 import ConnectButton from "./ConnectButton";
 import Link from "next/link";
+import PokerPlus from "./PokerPlus";
+import Chips from "./Chips";
 
 const LeandingPage = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,9 +14,36 @@ const LeandingPage = () => {
       <ConnectButton setIsOpen={setIsOpen} isOpen={isOpen} />
 
       <div className="flex items-center justify-center gap-4 bg-[#1d1d1d] p-2.5">
-        <Link href="#chips" className="uppercase bg-primary text-background font-bold px-5 py-1.5 rounded-4xl text-lg">Chips</Link>
-        <Link href="#gold" className="uppercase bg-primary text-background font-bold px-5 py-1.5 rounded-4xl text-lg">gold</Link>
+        <Link
+          href="#chips"
+          className="uppercase bg-primary text-background font-bold px-5 py-1.5 rounded-4xl text-lg"
+        >
+          Chips
+        </Link>
+        <Link
+          href="#gold"
+          className="uppercase bg-primary text-background font-bold px-5 py-1.5 rounded-4xl text-lg"
+        >
+          gold
+        </Link>
       </div>
+
+      <PokerPlus />
+
+      <div className="bg-primary py-10 lg:py-24">
+        <div className="container">
+          <p className="text-background/80 italic lg:text-lg">
+            ACTIVE ZYNGA POKER ACCOUNT REQUIRED. 20% more chips and gold
+            compared with same price purchases in device app store versions of
+            ZYNGA POKER. Chips and gold values shown after login are inclusive
+            of any purchase multipliers that are applicable to you.
+          </p>
+        </div>
+      </div>
+
+      <section id="chips">
+        <Chips />
+      </section>
     </div>
   );
 };
