adf108eac2bb1a299a1fdbcd352ad1b13b1a0c03
priyank <79801053+priyank2999@users.noreply.github.com>
2026-02-09 11:59:02 +0530

    changes

---
+++ app/success/page.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import { CheckCircle2 } from "lucide-react";
+
+export default function Success() {
+    const [countdown, setCountdown] = useState(500);
+
+    const redirectHome = () => {
+        window.location.href = "/";
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCountdown((prev) => {
+                if (prev <= 1) {
+                    redirectHome();
+                    return 0;
+                }
+                return prev - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="flex items-center justify-center h-screen bg-[url('../assets/background.jpg')] bg-cover bg-center bg-no-repeat">
+            <div className="px-10 py-12 max-w-md w-full text-center animate-fade-in">
+
+                <CheckCircle2 className="w-20 h-20 text-green-500 mx-auto mb-4 animate-bounce" />
+
+                <h1 className="text-3xl font-bold text-gray-900 mb-2">
+                    Payment Successful 🎉
+                </h1>
+
+                <p className="text-gray-600 mb-6">
+                    Your payment was completed securely.
+                </p>
+
+                <p className="text-sm text-gray-500 mb-6">
+                    Redirecting in <span className="font-semibold">{countdown}</span> seconds…
+                </p>
+
+                <Button variant="outline" className="text-xl text-primary font-semibold py-1.5! px-5.5 h-auto uppercase bg-gradient-to-b from-[#C9964E] via-[#E7BE7A] to-[#F7E3A3]" onClick={redirectHome}>OK</Button>
+                <p className="text-xs text-gray-400 mt-4">
+                    You can safely close this page.
+                </p>
+            </div>
+        </div>
+    );
+}
--- components/common/PokerPlus.tsx
+++ components/common/PokerPlus.tsx
@@ -1,30 +1,15 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-import BonusImage from "@/assets/images/bonus.avif"
-import Image from "next/image";
-
 const PokerPlus = () => {
   return (
-    <div className="grid lg:grid-cols-2 text-primary">
-      <div className="lg:col-span-1 max-w-[500px] lg:ml-32 w-full flex items-center justify-center">
-        <div className="flex flex-col gap-y-4 p-4 lg:p-8 items-center lg:items-start justify-center">
-          <h3 className="font-semibold text-4xl">
-            Lala gaming
-          </h3>
-          <p className="max-md:text-center">
-            Download Zynga Poker +plus for extra value on purchases! All your
-            progress, chips, and achievements transfer seamlessly.
-          </p>
-          <Button variant="outline" className="text-xl text-primary font-semibold py-1.5! px-5.5 h-auto uppercase bg-gradient-to-b from-[#C9964E] via-[#E7BE7A] to-[#F7E3A3]">CLAIM BONUS</Button>
-        </div>
-      </div>
-      <div className="lg:col-span-1">
-        <Image
-          src={BonusImage}
-          alt="Bonus Image"
-          width={800}
-          height={700}
-          className="max-h-[800px] h-full w-full object-cover bg-cover bg-center" />
+    <div className="w-full h-96 bg-[url('../assets/images/bonus.avif')] bg-cover bg-center bg-no-repeat opacity-80">
+      <div className="max-w-2xl mx-auto flex justify-center flex-col gap-4 h-full">
+        <h3 className="font-semibold text-4xl">
+          Lala gaming
+        </h3>
+        <p className="max-md:text-center">
+          Download Zynga Poker +plus for extra value on purchases! All your
+          progress, chips, and achievements transfer seamlessly.
+        </p>
+        {/* <Button variant="outline" className="text-xl text-primary font-semibold py-1.5! px-5.5 h-auto uppercase bg-gradient-to-b from-[#C9964E] via-[#E7BE7A] to-[#F7E3A3]">CLAIM BONUS</Button> */}
       </div>
     </div>
   );
